df781c0f63ccbfc73eb3e5d2e4d75882f7322f9f
Donna Hogan <ddhogan@users.noreply.github.com>
2020-03-01 11:39:31 -0500

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,5 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy({"_includes/img": "img"});
+  eleventyConfig.addPassthroughCopy("css");
+};
